f3de9570509e4704ffbb9e8f78fe609550f118ac
1677237936 <1677237936@qq.com>
2020-12-24 16:28:03 +0800

    ShellHook输出钩取到的消息

--- 电脑使用情况统计系统/ShellHook.py
+++ 电脑使用情况统计系统/ShellHook.py
@@ -21,10 +21,10 @@ def WindowProc(Hwnd,uMsg,wParam,lParam):
     if uMsg==msgShellHook:
         if wParam==1: #HSHELL_WINDOWCREATED
             pass
-            #print("HSHELL_WINDOWCREATED")
+            print("钩取到窗口创建消息")
         elif wParam==2: #HSHELL_WINDOWDESTROYED
             pass
-            #print("HSHELL_WINDOWDESTROYED")
+            print("钩取到窗口销毁消息")
         elif wParam==3: #HSHELL_ACTIVATESHELLWINDOW
             pass
         elif wParam==4: #HSHELL_WINDOWACTIVATED
@@ -33,14 +33,14 @@ def WindowProc(Hwnd,uMsg,wParam,lParam):
             pass
         elif wParam==6: #HSHELL_REDRAW
             GetForegroundInfo()
-            #print("HSHELL_REDRAW")
+            print("钩取到窗口重绘消息")
         elif wParam==7: #HSHELL_TASKMAN
             pass
         elif wParam==8: #HSHELL_LANGUAGE
             pass
         elif wParam==32772: #ACTIVATE
             GetForegroundInfo()
-            #print("ACTIVATE")
+            print("钩取到窗口激活消息")
         else:
             pass
     return win32gui.CallWindowProc(lpPrevWndProc,Hwnd,uMsg,wParam,lParam)
